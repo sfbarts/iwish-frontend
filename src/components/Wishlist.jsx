e90c568bdffa494f0769a79e40b7f88cf8bb7e94
sfbarts <sfbedu@gmail.com>
2023-09-21 08:49:32 -0400

    Add button to create new item row
    -Uses uuidv4 to generate temporary id

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import itemsService from '../services/items'
 import Item from './Item'
 
@@ -20,20 +21,35 @@ const Wishlist = () => {
     return
   }
 
+  //addEmptyRow function adds an item row to the array
+  const addEmptyRow = () => {
+    const wishlistId = items[0].wishlist.id
+    const newItem = {
+      name: '',
+      url: '',
+      price: '',
+      id: uuidv4(),
+      wishlist: wishlistId,
+    }
+
+    const newItems = items.concat(newItem)
+    setItems(newItems)
+    console.log(newItems)
+  }
+
   //Get the name of the wishlist and the total price
   const wishlistName = items[0].wishlist.name
   const total = items.reduce((sum, item) => sum + Number(item.price), 0)
-
-  console.log(total)
 
   return (
     <div>
       <h2>{wishlistName}</h2>
       <ul>
         {items.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} items={items} setItems={setItems} />
         ))}
       </ul>
+      <button onClick={addEmptyRow}>Add Item</button>
       <p>Total: {total}</p>
     </div>
   )
