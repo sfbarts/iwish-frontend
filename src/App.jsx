b78cd341290d67c747a10ca6e563c237441c832d
sfbarts <sfbedu@gmail.com>
2023-09-22 14:11:38 -0400

    Use react-router to route to each component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import Wishlist from './components/Wishlist'
+import Wishlists from './components/Wishlists'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+  useMatch,
+  useNavigate,
+} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <Wishlist />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/wishlists" element={<Wishlists />} />
+        <Route path="/wishlists/:id" element={<Wishlist />} />
+      </Routes>
+    </Router>
   )
 }
 
