7310b348c0f18ca293a19fbb1f61f6727b63b015
sfbarts <sfbedu@gmail.com>
2023-09-22 14:11:00 -0400

    Modify the getAll route to use the wishlist id

--- src/services/items.js
+++ src/services/items.js
@@ -3,10 +3,8 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:3001/api/items'
 
 //Get all items from a specific wishlist
-const getAll = async () => {
-  //temporary wishlist id
-  const tempWishlistId = '650a1a9d561cd48a13a4f05a'
-  const request = await axios.get(`${baseUrl}/${tempWishlistId}`)
+const getAll = async (wishlistId) => {
+  const request = await axios.get(`${baseUrl}/${wishlistId}`)
   return request.data
 }
 
