0ba2b541c7862200e42994d5bc432747e1332c3c
sfbarts <sfbedu@gmail.com>
2023-09-26 08:54:22 -0400

    Add NavBar to use on page layout

---
+++ src/components/navigation/desktop/NavBar.jsx
@@ -0,0 +1,14 @@
+import NavBarButtons from './NavBarButtons'
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav>
+        <p>Logo Placeholder</p>
+        <NavBarButtons />
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/components/navigation/desktop/NavBarButtons.jsx
@@ -0,0 +1,27 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import LoginButton from '../../buttons/login-button'
+import LogoutButton from '../../buttons/logout-button'
+import SignupButton from '../../buttons/signup-button'
+
+const NavBarButtons = () => {
+  const { isAuthenticated } = useAuth0()
+  console.log(isAuthenticated)
+
+  return (
+    <div className="nav-bar__buttons">
+      {!isAuthenticated && (
+        <>
+          <SignupButton />
+          <LoginButton />
+        </>
+      )}
+      {isAuthenticated && (
+        <>
+          <LogoutButton />
+        </>
+      )}
+    </div>
+  )
+}
+
+export default NavBarButtons
