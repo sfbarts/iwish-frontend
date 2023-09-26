664eafa9bac249e2838c67332b261f09946a2911
sfbarts <sfbedu@gmail.com>
2023-09-26 08:48:45 -0400

    Add buttons to handle login, signup and authentication logic

---
+++ src/components/buttons/logout-button.jsx
@@ -0,0 +1,21 @@
+import { useAuth0 } from '@auth0/auth0-react'
+
+const LogoutButton = () => {
+  const { logout } = useAuth0()
+
+  const handleLogout = () => {
+    logout({
+      logoutParams: {
+        returnTo: window.location.origin + '/login',
+      },
+    })
+  }
+
+  return (
+    <button className="button__logout" onClick={handleLogout}>
+      Log Out
+    </button>
+  )
+}
+
+export default LogoutButton
