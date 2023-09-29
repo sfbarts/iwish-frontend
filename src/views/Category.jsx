e5ef862b8a0092a461dbda42da187e4c951f9f36
sfbarts <sfbedu@gmail.com>
2023-09-28 22:21:42 -0400

    Use dispatch to reset links state whenever they are not needed

--- src/views/Category.jsx
+++ src/views/Category.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import PageLayout from '../components/PageLayout'
 import Wishlists from '../components/Wishlists'
+import { setWishlistLink } from '../reducers/wishlistLinkReducer'
 
 const CategoryView = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(setWishlistLink({ name: '', path: '' }))
+  })
+
   return (
     <PageLayout>
       <Wishlists />
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,22 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import PageLayout from '../components/PageLayout'
 import Categories from '../components/Categories'
+import { setCategoryLink } from '../reducers/categoryLinkReducer'
+import { setWishlistLink } from '../reducers/wishlistLinkReducer'
 
-const HomeView = () => (
-  <PageLayout>
-    <Categories />
-  </PageLayout>
-)
+const HomeView = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(setCategoryLink({ name: '', path: '' }))
+    dispatch(setWishlistLink({ name: '', path: '' }))
+  }, [])
+
+  return (
+    <PageLayout>
+      <Categories />
+    </PageLayout>
+  )
+}
 
 export default HomeView
