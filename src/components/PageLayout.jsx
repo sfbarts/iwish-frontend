db0b70c67d19bd5aa824f5813e248c0c376a9f7b
sfbarts <sfbedu@gmail.com>
2023-09-28 22:23:07 -0400

    Include Breadcrumbs in page layout

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,9 +1,11 @@
 import NavBar from './navigation/desktop/NavBar'
+import Breadcrumbs from './navigation/desktop/Breadcrumbs'
 
 const PageLayout = ({ children }) => {
   return (
     <div>
       <NavBar />
+      <Breadcrumbs />
       <div>{children}</div>
     </div>
   )
