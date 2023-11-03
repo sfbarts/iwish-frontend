55abc81495d98cc5970bb187826516a1393b8bad
sfbarts <sfbedu@gmail.com>
2023-11-03 09:28:44 -0400

    Update page layout for overal page structure.

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,14 +1,16 @@
 import NavBar from './navigation/desktop/NavBar'
 import Breadcrumbs from './navigation/desktop/Breadcrumbs'
 import Notification from './Notification'
+import Footer from './navigation/desktop/Footer'
 
 const PageLayout = ({ children }) => {
   return (
-    <div>
+    <div className="page-layout">
       <NavBar />
       <Breadcrumbs />
       <Notification />
-      <div>{children}</div>
+      <div className="main-content">{children}</div>
+      <Footer />
     </div>
   )
 }
