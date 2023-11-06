dac12e6578124f6f876f90c8251a27a2b256d862
sfbarts <sfbedu@gmail.com>
2023-11-06 14:15:59 -0500

    Update styling for welcome layout

--- src/components/WelcomeCard.jsx
+++ src/components/WelcomeCard.jsx
@@ -1,14 +1,18 @@
 import LoginButton from './buttons/login-button'
 import SignupButton from './buttons/signup-button'
+import Footer from './navigation/desktop/Footer'
 
 const WelcomeCard = ({ children }) => {
   return (
-    <div className="welcome-card">
-      <>{children}</>
-      <div className="auth-buttons">
-        <SignupButton /> - or -
-        <LoginButton />
+    <div className="welcome-layout">
+      <div className="welcome-card">
+        <>{children}</>
+        <div className="auth-buttons">
+          <SignupButton /> - or -
+          <LoginButton />
+        </div>
       </div>
+      <Footer />
     </div>
   )
 }
