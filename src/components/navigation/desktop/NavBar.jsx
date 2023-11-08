47b342d6af4c90759d6bdcf305d99f74b23b554e
sfbarts <sfbedu@gmail.com>
2023-11-08 10:07:51 -0500

    Added link to iWish Logo

--- src/components/navigation/desktop/NavBar.jsx
+++ src/components/navigation/desktop/NavBar.jsx
@@ -1,13 +1,17 @@
+import { Link } from 'react-router-dom'
 import LogoutButton from '../../buttons/logout-button'
 
 const NavBar = () => {
   return (
     <header className="nav-bar">
-      <img
-        className="logo"
-        src="/assets/IWish_Logo_600x206.png"
-        loading="lazy"
-      />
+      <Link to="/">
+        <img
+          className="logo"
+          src="/assets/IWish_Logo_600x206.png"
+          loading="lazy"
+        />
+      </Link>
+
       <nav className="user-menu">
         <LogoutButton />
       </nav>
