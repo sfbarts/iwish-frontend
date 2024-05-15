887ae7c446ce7f2c904b00370f30e68631de6f76
sfbarts <sfbedu@gmail.com>
2024-05-15 17:50:46 -0400

    Refactor Item component to use the modifiedList reducer. It simplifies the management of the items state.

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,19 +1,19 @@
 import { useState, useEffect } from 'react'
 import Checkbox from '@mui/material/Checkbox'
+import { useDispatch } from 'react-redux'
+import { setModifiedList } from '../reducers/modifiedListReducer'
 import itemsService from '../services/items'
 import Tooltip from './CustomTooltip'
 
 const Item = ({ item, items, setItems, accessToken }) => {
-  //originalItem holds the original values for each item input
-  const [originalItem, setOriginalItem] = useState({})
   //newItem state now controls all the inputs of each item
   const [newItem, setNewItem] = useState(null)
 
+  const dispatch = useDispatch()
+
   //Set the item price input to be empty by default if it is 0
-  //Save original item using item passed from Wishlist component
   useEffect(() => {
     setNewItem({ ...item, price: item.price > 0 ? item.price : '' })
-    setOriginalItem(item)
   }, [item])
 
   //only render once newItem has beens set
@@ -21,41 +21,16 @@ const Item = ({ item, items, setItems, accessToken }) => {
     return
   }
 
-  //Items state is updated if items have changed in order to keep list updated.
-  const addToItems = (updatedItem) => {
-    if (updatedItem !== originalItem) {
-      const newItems = items.map((item) =>
-        item.id !== updatedItem.id ? item : updatedItem
-      )
-      setItems(newItems)
-      setOriginalItem(updatedItem)
+  // handleChangeUpdate() triggers whenever a change is made on the item.
+  // It currently updates the item state and dispatches the item to the modifiedItems reducer.
+  const handleChangeUpdate = (e) => {
+    const { name, value, checked } = e.target
+    const updatedItem = {
+      ...newItem,
+      [name]: name !== 'acquired' ? value : checked,
     }
-  }
-
-  //handleNewName controls name input value and sets newItem state
-  const handleNameUpdate = (e) => {
-    const newName = { ...newItem, name: e.target.value }
-    setNewItem(newName)
-  }
-
-  //handleUrlUpdate controls URL input value and sets newItem state
-  const handleUrlUpdate = (e) => {
-    const newUrl = { ...newItem, url: e.target.value }
-    setNewItem(newUrl)
-  }
-
-  //handlePriceUpdate controls price input value and sets newItem state
-  const handlePriceUpdate = (e) => {
-    const newPrice = e.target.value
-    setNewItem({ ...newItem, price: newPrice })
-    addToItems({ ...newItem, price: newPrice })
-  }
-
-  //handleAcquiredUpdate controls acquired input value and sets newItem state
-  const handleAcquiredUpdate = (e) => {
-    const newAcquired = { ...newItem, acquired: e.target.checked }
-    setNewItem(newAcquired)
-    addToItems(newAcquired)
+    setNewItem(updatedItem)
+    dispatch(setModifiedList(updatedItem))
   }
 
   //handleRemoveItem removes item from database.
@@ -70,28 +45,30 @@ const Item = ({ item, items, setItems, accessToken }) => {
       <input
         className="input-item regular"
         type="text"
+        name="name"
         value={newItem.name}
-        onChange={handleNameUpdate}
-        onBlur={() => addToItems(newItem)}
+        onChange={handleChangeUpdate}
         maxLength={100}
       />
       <input
         className="input-item regular"
         type="text"
+        name="url"
         value={newItem.url}
-        onChange={handleUrlUpdate}
-        onBlur={() => addToItems(newItem)}
+        onChange={handleChangeUpdate}
         maxLength={400}
       />
       <input
         className="input-item regular item-price"
         type="number"
+        name="price"
         value={newItem.price}
-        onChange={handlePriceUpdate}
+        onChange={handleChangeUpdate}
         placeholder="0.0"
       />
       <Checkbox
-        onChange={handleAcquiredUpdate}
+        name="acquired"
+        onChange={handleChangeUpdate}
         checked={newItem.acquired}
         sx={{
           color: 'white',
