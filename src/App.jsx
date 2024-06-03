71106c7fc28decc1a856807d94217dcbed7081fb
sfbarts <sfbedu@gmail.com>
2024-06-02 20:51:09 -0400

    Change beforeunload for visibilitychange

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ const App = () => {
         dispatch(saveWishlist(aT))
       }
 
-      window.addEventListener('beforeunload', handleBeforeUnload)
+      window.addEventListener('visibilitychange', handleBeforeUnload)
     }
   }, [isAuthenticated, aT])
 
